47da23825d11851cf6fa2258713d10f2d99dee15
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-01-16 21:47:39 +0500

    Company register to'liq bo'ldi

--- app/Http/Controllers/TelegramRegistrationController.php
+++ app/Http/Controllers/TelegramRegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -35,7 +36,7 @@ class TelegramRegistrationController extends Controller
         } elseif (isset($data['message']['photo'])) {
             return 'photo';
         }
-        return null;
+        return 'other';
     }
 
     private function getStep(int $chatId, string $data)
@@ -80,6 +81,16 @@ class TelegramRegistrationController extends Controller
         ]);
     }
 
+    private function sendCompanyInfo(int $chatId, $company)
+    {
+        Http::post($this->telegramApiUrl . 'sendMessage', [
+            'chat_id' => $chatId,
+            'text' => "Company's name: {$company['name']}\n" .
+                "Longitude: {$company['longitude']}\n" .
+                "Latitude: {$company['latitude']}\n",
+        ]);
+    }
+
     private function getFile($fileId)
     {
         $url = $this->telegramApiUrl . "getFile";
@@ -100,6 +111,23 @@ class TelegramRegistrationController extends Controller
         cache()->put("photo_path_{$chatId}", "images/$uniqId.jpg");
     }
 
+    private function deleteMessage($chatId, $messageId)
+    {
+        Http::post($this->telegramApiUrl . 'deleteMessage', [
+            'chat_id' => $chatId,
+            'message_id' => $messageId
+        ]);
+    }
+
+    private function getMessageId($data)
+    {
+        if (isset($data['message']['message_id'])) {
+            return $data['message']['message_id'];
+        } elseif (isset($data['callback_query']['message']['message_id'])) {
+            return $data['callback_query']['message']['message_id'];
+        }
+    }
+
     public function handle(Request $request)
     {
         $data = $request->all();
@@ -110,12 +138,15 @@ class TelegramRegistrationController extends Controller
 
         $step = $this->getStep($chatId, $chatData);
 
-        // Log::info([$chatId, $chatData, $data, $step]);
+        $messageId = $this->getMessageId($data);
+
+        // Log::info([$chatId, $chatData, $data, $step, $messageId]);
 
         switch ($step) {
             case 'start':
             case '/start':
                 $this->sendOptions($chatId);
+                $this->deleteMessage($chatId, $messageId);
                 cache()->put("registration_step_{$chatId}", 'registration');
                 break;
 
@@ -124,6 +155,9 @@ class TelegramRegistrationController extends Controller
                 $regStepAs = cache()->get("registration_step_as_{$chatId}");
 
                 if ($chatData == 'company' || $regStepAs == 'company') {
+
+                    $this->deleteMessage($chatId, $messageId - 1);
+                    $this->deleteMessage($chatId, $messageId);
 
                     cache()->put("registration_step_as_{$chatId}", 'company');
 
@@ -139,7 +173,11 @@ class TelegramRegistrationController extends Controller
                             break;
                         case 'name':
 
+                            $this->deleteMessage($chatId, $messageId - 1);
+                            $this->deleteMessage($chatId, $messageId);
+
                             cache()->put("company_name_{$chatId}", $chatData);
+
                             $this->sendMessage($chatId, "Please, send a photo for your company!");
 
                             cache()->put("registration_company_{$chatId}", 'logo');
@@ -149,26 +187,51 @@ class TelegramRegistrationController extends Controller
 
                             if ($chatData == 'photo') {
 
+                                $this->deleteMessage($chatId, $messageId - 1);
+                                $this->deleteMessage($chatId, $messageId);
+
                                 $this->getPhotoAndStore($chatId, $data);
 
                                 $this->sendMessage($chatId, "Now, please send your company's location longitude(Uzunlik):");
 
                                 cache()->put("registration_company_{$chatId}", 'longitude');
                             } else {
+                                $this->deleteMessage($chatId, $messageId - 1);
+                                $this->deleteMessage($chatId, $messageId);
                                 $this->sendMessage($chatId, "Please, send a photo for your company, nothing else!😌");
                             }
                             break;
                         case 'longitude':
 
+                            $this->deleteMessage($chatId, $messageId - 1);
+                            $this->deleteMessage($chatId, $messageId);
+
+                            cache()->put("company_longitude_{$chatId}", $chatData);
+
                             $this->sendMessage($chatId, "Now, please send your company's location latitude(Kenglik):");
 
                             cache()->put("registration_company_{$chatId}", 'latitude');
+
                             break;
                         case 'latitude':
 
+                            $this->deleteMessage($chatId, $messageId - 1);
+                            $this->deleteMessage($chatId, $messageId);
+
                             $this->sendMessage($chatId, "Your company has been created. Now you need to register for yourself!");
 
-                            // cache()->put("registration_company_{$chatId}", 'longitude');
+                            $company = Company::create([
+                                'name' => cache()->get("company_name_{$chatId}"),
+                                'logo' => cache()->get("photo_path_{$chatId}"),
+                                'longitude' => cache()->get("company_longitude_{$chatId}"),
+                                'latitude' => $chatData
+                            ]);
+                            $company = Company::where('id', $company->id)->first();
+                            $this->sendCompanyInfo($chatId, $company);
+
+                            cache()->forget("registration_company_{$chatId}");
+                            cache()->put("registration_step_as_{$chatId}", 'employee');
+
                             break;
                     }
                 } elseif ($chatData == 'employee' || $regStepAs == 'employee') {
